3d62e1248a8b7591467b6f6be1e3738eeb045260
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-08 16:51:42 +0900

    add study

---
+++ 02 자바스크립트/javascript_practice/app.js
@@ -0,0 +1,9 @@
+// 3가지 타입의 변수 지정 방법 : const, let, var
+const mackbook = "300";
+let ipad = "100";
+// var는 오래된 변수 지정 방법이다. 
+var iphone = "200";
+
+console.log("mackbook", mackbook);
+console.log("ipad", ipad);
+console.log("iphone", iphone);
