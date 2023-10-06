fb92c4101707d4465f8a34adc9b91803717e0434
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-10-06 15:00:24 +0900

    form 태그와 POST API 실습 (3)

--- 01 사전 학습/01 Big Picture/당근 마켓 클론 코딩/frontend/write.js
+++ 01 사전 학습/01 Big Picture/당근 마켓 클론 코딩/frontend/write.js
@@ -1,11 +1,20 @@
 const form = document.getElementById("write-form");
 
-const handleSubmitForm = (event) => {
+const handleSubmitForm = async (event) => {
     event.preventDefault();
-    fetch('/items', {
-        method: 'POST',
-        body: new FormData(form),
-    })
+    // try 안에 코드를 실행해보다가 error가 발생하면 
+    try {
+        const res = await fetch('/items', {
+            method: 'POST',
+            body: new FormData(form),
+        });
+        // 이 밑의 코드가 실행이 되어라 
+    } catch (e) {
+        const data = await res.json();
+        // window 객체 참고하여 pathname을 root로 바꾸기 
+        if (data === '200') window.location.pathname = "/";
+    }
+    console.log(e);
 };
 
 form.addEventListener("submit", handleSubmitForm);
--- 01 사전 학습/01 Big Picture/당근 마켓 클론 코딩/main.py
+++ 01 사전 학습/01 Big Picture/당근 마켓 클론 코딩/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI, UploadFile, Form 
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
+import sqlite3
+
+con = sqlite3.connect('db.db', check_same_thread=False)
+# db에서 cursor를 이용해서 특정 인서트하거나 셀렉트 할때 사용
+cur = con.cursor()
 
 app = FastAPI()
 
 @app.post('/items')
-def create_item(
+async def create_item(
     image:UploadFile, 
     title:Annotated[str,Form()], 
     price:Annotated[int,Form()], 
     description:Annotated[str,Form()], 
     place:Annotated[str,Form()]
     ):
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO items(title, image, price, description, place)
+                VALUES ('{title}', ''{image_bytes.hex()}', '{price}', '{description}', '{place}')
+                """)
+    # 데이터가 들어감 
+    con.commit()
+    return '200'
 
 
 
